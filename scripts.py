8e7ac3bdcce5d9044bf5910f6b67922a9a9f3ad8
Alex <soyvitaly@gmail.com>
2021-10-01 16:07:27 +0200

    Вынес копипасту в функцию

--- scripts.py
+++ scripts.py
@@ -3,32 +3,33 @@ from datacenter.models import Lesson, Schoolkid, Chastisement, Mark, Commendatio
 import random
 
 
-def fix_marks(schoolkid_name):
+def check_schoolkid(schoolkid_name):
     try:
         schoolkid = Schoolkid.objects.get(full_name__contains=schoolkid_name)
-        bad_marks = Mark.objects.filter(schoolkid=schoolkid,
-                                        points__in=[1, 2, 3])
-        for mark in bad_marks:
-            mark.points = 5
-            mark.save()
-        print(f'У "{schoolkid.full_name}" не осталось плохих отметок')
+        return schoolkid
     except ObjectDoesNotExist:
         print(f'{schoolkid_name} не найден')
     except MultipleObjectsReturned:
         print(f'Hайдено больше 1ого ученика с именем {schoolkid_name}')
+
+
+def fix_marks(schoolkid_name):
+    schoolkid = check_schoolkid(schoolkid_name)
+    bad_marks = Mark.objects.filter(schoolkid=schoolkid,
+                                    points__in=[1, 2, 3])
+    for mark in bad_marks:
+        mark.points = 5
+        mark.save()
+    print(f'У "{schoolkid.full_name}" не осталось плохих отметок')
 
 
 def remove_chastisements(schoolkid_name):
-    try:
-        schoolkid = Schoolkid.objects.get(full_name__contains=schoolkid_name)
-        schoolkid_chastisements = Chastisement.objects.filter(
-            schoolkid=schoolkid)
-        schoolkid_chastisements.delete()
-        print(f'У {schoolkid.full_name} удалены все замечания')
-    except ObjectDoesNotExist:
-        print(f'{schoolkid_name} не найден')
-    except MultipleObjectsReturned:
-        print(f'Hайдено больше 1ого ученика с именем {schoolkid_name}')
+    schoolkid = check_schoolkid(schoolkid_name)
+    schoolkid_chastisements = Chastisement.objects.filter(
+        schoolkid=schoolkid)
+    schoolkid_chastisements.delete()
+    print(f'У {schoolkid.full_name} удалены все замечания')
+    
 
 
 def create_commendation(schoolkid_name, subject_name):
@@ -52,24 +53,20 @@ def create_commendation(schoolkid_name, subject_name):
                      'Ты растешь над собой!',
                      'Ты многое сделал, я это вижу!',
                      'Теперь у тебя точно все получится!']
-    try:
-        schoolkid = Schoolkid.objects.get(full_name__contains=schoolkid_name)
-        lessons = Lesson.objects.filter(year_of_study=schoolkid.year_of_study,
-                                        group_letter=schoolkid.group_letter,
-                                        subject__title=subject_name).order_by(
-            '-date')
-        lesson = lessons.first()
-        commendation = random.choice(commendations)
-        if lesson:
-            Commendation.objects.create(
-                schoolkid=schoolkid,
-                created=lesson.date,
-                subject=lesson.subject,
-                teacher=lesson.teacher,
-                text=commendation
-            )
-            print(f'Создана похвала: "{commendation}"')
-    except ObjectDoesNotExist:
-        print(f'{schoolkid_name} не найден')
-    except MultipleObjectsReturned:
-        print(f'Hайдено больше 1ого ученика с именем {schoolkid_name}')
+
+    schoolkid = check_schoolkid(schoolkid_name)
+    lessons = Lesson.objects.filter(year_of_study=schoolkid.year_of_study,
+                                    group_letter=schoolkid.group_letter,
+                                    subject__title=subject_name).order_by(
+        '-date')
+    lesson = lessons.first()
+    commendation = random.choice(commendations)
+    if lesson:
+        Commendation.objects.create(
+            schoolkid=schoolkid,
+            created=lesson.date,
+            subject=lesson.subject,
+            teacher=lesson.teacher,
+            text=commendation
+        )
+        print(f'Создана похвала: "{commendation}"')
